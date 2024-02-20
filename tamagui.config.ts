d78a68f67ed30e685bfe5255704c08803ca2d5aa
Sofia Rest <srest2021@gmail.com>
2024-02-20 18:46:03 -0500

    tamagui config

--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,13 +1,30 @@
-import { themes, tokens } from '@tamagui/themes'
-import { createTamagui } from 'tamagui'
+// import { themes, tokens } from '@tamagui/themes'
+// import { createTamagui } from 'tamagui'
 
-const appConfig = createTamagui({
-  themes,
-  tokens
-})
+// const appConfig = createTamagui({
+//   themes,
+//   tokens
+// })
+
+// export type AppConfig = typeof appConfig
+// declare module '@tamagui/core' {
+//   interface TamaguiCustomConfig extends AppConfig {}
+// }
+// export default appConfig
+
+import { config } from '@tamagui/config/v3'
+import { Text, View } from 'react-native'
+import { createTamagui } from 'tamagui' // or '@tamagui/core'
+
+const appConfig = createTamagui(config)
 
 export type AppConfig = typeof appConfig
-declare module '@tamagui/core' {
+
+declare module 'tamagui' {
+  // or '@tamagui/core'
+  // overrides TamaguiCustomConfig so your custom types
+  // work everywhere you import `tamagui`
   interface TamaguiCustomConfig extends AppConfig {}
 }
+
 export default appConfig
