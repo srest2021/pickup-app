3595f91cabdc8873474124b898fbf98509b97ca3
Sofia Rest <srest2021@gmail.com>
2024-02-27 12:26:30 -0500

    merged with use-query-games; added selectedGameId

--- src/hooks/use-mutation-game.tsx
+++ src/hooks/use-mutation-game.tsx
@@ -5,24 +5,26 @@ import { Alert } from "react-native";
 import { Game, GameSport } from "../lib/types";
 
 function useMutationGame() {
-  const [session, setSession, setLoading, setUpdateGameStatus, addMyGame] =
-    useStore((state) => [
+  const [session, setLoading, setUpdateGameStatus, addMyGame, removeMyGame] = useStore(
+    (state) => [
       state.session,
-      state.setSession,
+      //state.setSession,
       state.setLoading,
       state.setUpdateGameStatus,
       state.addMyGame,
-    ]);
+      state.removeMyGame
+    ],
+  );
 
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
+  // useEffect(() => {
+  //   supabase.auth.getSession().then(({ data: { session } }) => {
+  //     setSession(session);
+  //   });
 
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-  }, []);
+  //   supabase.auth.onAuthStateChange((_event, session) => {
+  //     setSession(session);
+  //   });
+  // }, []);
 
   const createGame = async (
     game_title: string,
@@ -52,39 +54,59 @@ function useMutationGame() {
           },
         ])
         .select();
+      if (error) throw error;
 
       // Successful game creation.
       setUpdateGameStatus(true);
 
-      // Temporary work around.
-      let gameId = null;
-      if (data) {
-        gameId = data[0].id;
-      } else {
-        throw new Error("Error adding game; please try again")
+      if (data && data[0]) {
+        // add game to store
+        const myNewGame: Game = {
+          id: data[0].id,
+          title: game_title,
+          description: description,
+          datetime: datetime,
+          address: address,
+          sport: { name: sport, skillLevel: skillLevel } as GameSport,
+          maxPlayers: Number(playerLimit),
+        };
+        addMyGame(myNewGame);
+        return myNewGame.id;
       }
-
-      // add game to store
-      const myNewGame: Game = {
-        id: gameId,
-        title: game_title,
-        description: description,
-        datetime: datetime,
-        address: address,
-        sport: { name: sport, skillLevel: skillLevel } as GameSport,
-        maxPlayers: Number(playerLimit),
-      };
-      addMyGame(myNewGame);
     } catch (error) {
       if (error instanceof Error) {
         Alert.alert(error.message);
       }
+      return null;
     } finally {
       setLoading(false);
     }
   };
 
-  return { createGame };
+  const removeGameById = async (id: string) => {
+    try {
+      setLoading(true);
+      if (!session?.user) throw new Error("No user on the session!");
+      
+      const { error } = await supabase
+        .from("games")
+        .delete()
+        .eq("id", id);
+      if (error) throw error;
+        
+      // remove from store
+      removeMyGame(id);
+    } catch (error) {
+      if (error instanceof Error) {
+        Alert.alert(error.message);
+      }
+      return null;
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return { createGame, removeGameById };
 }
 
 export default useMutationGame;
--- src/hooks/use-query-games.tsx
+++ src/hooks/use-query-games.tsx
@@ -1,80 +1,127 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useStore } from "../lib/store";
 import { supabase } from "../lib/supabase";
 import { Alert } from "react-native";
+import { Game, GameSport } from "../lib/types";
 
 function useQueryGames() {
-  const [session, setSession, setLoading, setMyGames] = useStore(
-    (state) => [
-      state.session,
-      state.setSession,
-      state.setLoading,
-      state.setMyGames,
-    ]
-  );
+  const [
+    session,
+    setLoading,
+    setMyGames,
+    setSelectedGameId,
+    clearSelectedGameId,
+  ] = useStore((state) => [
+    state.session,
+    state.setLoading,
+    state.setMyGames,
+    state.setSelectedGameId,
+    state.clearSelectedGameId,
+  ]);
+
+  const [game, setGame] = useState<Game | null>(null);
+
+  const fetchGameById = async (id: string) => {
+    try {
+      setLoading(true);
+      if (!session?.user) throw new Error("Please sign in to view a game");
+
+      const { data, error } = await supabase
+        .from("games")
+        .select("*")
+        .eq("id", id);
+      if (error) throw error;
+      //console.log(data);
+
+      if (data && data[0]) {
+        const game: Game = {
+          id: data[0].id,
+          title: data[0].title,
+          description: data[0].description,
+          datetime: data[0].datetime,
+          address: data[0].address,
+          sport: {
+            name: data[0].sport,
+            skillLevel: data[0].skill_level,
+          } as GameSport,
+          maxPlayers: Number(data[0].max_players),
+        };
+        console.log("fetched game: ", game)
+
+        setGame(game);
+        setSelectedGameId(game.id);
+        return game;
+      } else {
+        throw new Error("Error loading game; please try again");
+      }
+    } catch (error) {
+      setGame(null);
+      clearSelectedGameId();
+      if (error instanceof Error) {
+        Alert.alert(error.message);
+      }
+      return null;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const fetchMyGames = async () => {
+    try {
+      setLoading(true);
+      if (!session?.user) throw new Error("Please sign in to view games");
+
+      const { data: games, error } = await supabase
+        .from("games")
+        .select("*")
+        .eq("organizer_id", session.user.id);
+      if (error) throw error;
+
+      if (games) {
+        setMyGames(games);
+        console.log("fetched games: ", games);
+      } 
+    } catch (error) {
+      if (error instanceof Error) {
+        Alert.alert(error.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const fetchAllGames = async () => {
+    try {
+      setLoading(true);
+
+      //TODO: ADD PAGINATION - right now only returning 20 most relevant games
+      const { data: games, error } = await supabase
+        .from("games")
+        .select("*")
+        .limit(20);
+      if (error) throw error;
+
+      if (games) {
+        setMyGames(games);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        Alert.alert(error.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchMyGames = async () => {
-      try {
-        setLoading(true);
-        if (!session?.user) throw new Error("Please sign in to view games");
-
-        const { data: games, error } = await supabase
-          .from("games")
-          .select("*")
-          .eq("organizer_id", session.user.id);
-
-        if (error) {
-          throw error;
-        }
-
-        if (games) {
-          setMyGames(games);
-        }
-      } catch (error) {
-        if (error instanceof Error) {
-          Alert.alert(error.message);
-        }
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    const fetchAllGames = async () => {
-      try {
-        setLoading(true);
-
-        //TODO: ADD PAGINATION - right now only returning 20 most relevant games
-        const { data: games, error } = await supabase.from("games").select("*").limit(20);
-
-        if (error) {
-          throw error;
-        }
-
-        if (games) {
-          // Update the state with all games
-          // You might want to update the state differently for all games
-          // Maybe you want to distinguish between user's games and all games
-          // You can adjust this part based on your application's requirements
-          setMyGames(games);
-        }
-      } catch (error) {
-        if (error instanceof Error) {
-          Alert.alert(error.message);
-        }
-      } finally {
-        setLoading(false);
-      }
-    };
-
+    //fetchGame("37bc2b67-020e-4b58-911a-c6a1864954f5"); //test
     if (session?.user) {
       fetchMyGames();
     }
-    
     fetchAllGames();
-  }, [session, setMyGames, setLoading]);
+  }, []);
 
-  return {};
+  return { fetchGameById, fetchMyGames, fetchAllGames };
 }
 
 export default useQueryGames;
