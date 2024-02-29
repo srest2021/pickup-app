2d8114173553a845f614c37384dda18ed457c796
Sofia Rest <srest2021@gmail.com>
2024-02-29 12:50:28 -0500

    todo: edit and delete buttons

--- src/components/game/MyGameView.tsx
+++ src/components/game/MyGameView.tsx
@@ -8,6 +8,8 @@ import {
   XStack,
   Label,
   Text,
+  Button,
+  ScrollView,
 } from "tamagui";
 import { useStore } from "../../lib/store";
 import { View } from "react-native";
@@ -49,105 +51,89 @@ const MyGameView = ({ navigation, route }: { navigation: any; route: any }) => {
   return (
     <View>
       {session && session.user && user ? (
-        <View className="p-12">
-          <YStack>
-            <YStack alignItems="center">
-              <H4 textAlign="center">{game.title}</H4>
-            </YStack>
-
-            <YStack paddingTop="$3" alignItems="center">
-              <H5>{displayDate}</H5>
-              <H5>at {displayTime}</H5>
-            </YStack>
-
-            <YStack alignItems="center">
-              <SizableText alignItems="center" padding="$5" size="$4">
-                by @{user.username}
-              </SizableText>
-            </YStack>
-
-            {description && (
-              <YStack paddingTop="$3" paddingBottom="$7">
-                <Card elevate size="$5">
-                  <View marginLeft={25} marginRight={25}>
-                    <SizableText
-                      size="$5"
-                      fontWeight="500"
-                      paddingTop="$3"
-                      paddingBottom="$3"
-                    >
-                      {description}
-                    </SizableText>
-                  </View>
-                </Card>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View className="p-12">
+            <YStack>
+              <YStack alignItems="center">
+                <H4 textAlign="center">{game.title}</H4>
               </YStack>
-            )}
 
-            <XStack space="$2" alignItems="center">
-              <Label size="$5" width={90}>
-                Address
-              </Label>
-              <SizableText flex={1} size="$5">
-                {game.address}
-              </SizableText>
-            </XStack>
+              <YStack paddingTop="$3" alignItems="center">
+                <H5>{displayDate}</H5>
+                <H5>at {displayTime}</H5>
+              </YStack>
 
-            <XStack space="$2" alignItems="center">
-              <Label size="$5" width={90}>
-                Sport
-              </Label>
-              <SizableText flex={1} size="$5">
-                {game.sport.name}
-              </SizableText>
-            </XStack>
+              <YStack alignItems="center">
+                <SizableText alignItems="center" padding="$5" size="$4">
+                  by @{user.username}
+                </SizableText>
+              </YStack>
 
-            <XStack space="$2" alignItems="center">
-              <Label size="$5" width={90}>
-                Skill Level
-              </Label>
-              <SizableText flex={1} size="$5">
-                {getSkillLevelString(game.sport.skillLevel)}
-              </SizableText>
-            </XStack>
-          </YStack>
-        </View>
+              {description && (
+                <YStack paddingTop="$3" paddingBottom="$7">
+                  <Card elevate size="$5">
+                    <View marginLeft={25} marginRight={25}>
+                      <SizableText
+                        size="$5"
+                        fontWeight="500"
+                        paddingTop="$3"
+                        paddingBottom="$3"
+                      >
+                        {description}
+                      </SizableText>
+                    </View>
+                  </Card>
+                </YStack>
+              )}
+
+              <XStack space="$2" alignItems="center">
+                <Label size="$5" width={90}>
+                  Address
+                </Label>
+                <SizableText flex={1} size="$5">
+                  {game.address}
+                </SizableText>
+              </XStack>
+
+              <XStack space="$2" alignItems="center">
+                <Label size="$5" width={90}>
+                  Sport
+                </Label>
+                <SizableText flex={1} size="$5">
+                  {game.sport.name}
+                </SizableText>
+              </XStack>
+
+              <XStack space="$2" alignItems="center">
+                <Label size="$5" width={90}>
+                  Skill Level
+                </Label>
+                <SizableText flex={1} size="$5">
+                  {getSkillLevelString(game.sport.skillLevel)}
+                </SizableText>
+              </XStack>
+
+              <XStack space="$3" paddingTop="$5">
+                <Button
+                  theme="active"
+                  flex={1}
+                  onPress={() => navigation.navigate("EditGame", { game })}
+                >
+                  Edit (FIX ME)
+                </Button>
+                <Button theme="active" flex={1}>
+                  Delete (FIX ME)
+                </Button>
+              </XStack>
+            </YStack>
+          </View>
+        </ScrollView>
       ) : (
         <View className="p-12 text-center items-center flex-1 justify-center">
           <H4>Log in to view and edit this game!</H4>
         </View>
       )}
     </View>
-
-    // <YStack
-    //   position="relative"
-    //   padding="$4"
-    //   space="$2"
-    //   backgroundColor="$background"
-    //   borderRadius={10}
-    //   alignItems="center"
-    // >
-    //   {/* Edit button positioned on the top right of the view */}
-    //   <Button
-    //     position="absolute"
-    //     top={0}
-    //     right={0}
-    //     size="$2"
-    //     onPress={() => navigation.navigate("EditGame", { game })}
-    //   >
-    //     Edit
-    //   </Button>
-
-    //   <Text fontWeight="bold">{title}</Text>
-    //   <Text fontStyle="italic">{displayDate}</Text>
-    //   <Text>Sport: {sport.name}</Text>
-    //   <Text>Skill Level: {sport.skillLevel}</Text>
-    //   <Text>Max Players: {maxPlayers}</Text>
-    //   <Text>Address: {address}</Text>
-    //   <YStack>
-    //     <Text>Description:</Text>
-    //     <Text>{description}</Text>
-    //   </YStack>
-    // </YStack>
   );
 };
 
