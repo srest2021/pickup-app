bf751581ddc3e14892943307de291e25335232a3
Clarissa <msclarissa8@yahoo.com>
2024-03-02 17:25:43 -0500

    testing now working editGame test

--- __tests__/components/game/EditGame.test.js
+++ __tests__/components/game/EditGame.test.js
@@ -19,7 +19,7 @@ jest.mock("../../../src/hooks/use-mutation-game", () => () => ({
   editGameById: mockEditGameById,
 }));
 
-const testSport = {
+const mockSport = {
   name: "Basketball",
   skillLevel: "Beginner"
 }
@@ -32,7 +32,7 @@ const mockSelectedMyGame = {
   city: "Baltimore",
   state: "MD",
   zip: "21218",
-  sport: testSport,
+  sport: mockSport,
   maxPlayers: 20,
   description: "Test Description",
 };
@@ -43,70 +43,41 @@ jest.mock('../../../src/lib/store', () => ({
 }));
 
 
+
 describe('EditGame', () => {
-it('renders without crashing', () => {
-  const navigation = { goBack: jest.fn() };
-  const route = { params: { gameId: "gameId" } };
+  it('should call editGameById with updated attributes and navigate back when "Edit" button is pressed', async () => {
+    const navigation = { goBack: jest.fn() };
+    const route = { params: { gameId: "gameId" } };
 
-  render(
-    <TamaguiProvider config={appConfig}>
-      <EditGame navigation={navigation} route={route} />
-    </TamaguiProvider>
-  );
-
-  // If we reach this point without throwing an error, the component has rendered successfully
-  expect(true).toBeTruthy();
+    const { getByText, getByTestId } = render(
+      <TamaguiProvider config={appConfig}>
+        <EditGame navigation={navigation} route={route} />
+      </TamaguiProvider>
+    );
+  
+    // getByTestId to select the input field
+    fireEvent.changeText(getByTestId("titleInput"), "New Title");
+    fireEvent.press(getByText("Edit"));
+  
+    await waitFor(() => {
+      expect(mockEditGameById).toHaveBeenCalledWith(
+        "gameId",
+        "New Title", // only updating title
+        expect.any(Date),
+        mockSelectedMyGame.address,
+        mockSelectedMyGame.city,
+        mockSelectedMyGame.state,
+        mockSelectedMyGame.zip,
+        mockSelectedMyGame.sport.name,
+        expect.any(Number), 
+        mockSelectedMyGame.maxPlayers.toString(),
+        mockSelectedMyGame.description,
+      );
+    });
+  
+    expect(navigation.goBack).toHaveBeenCalled();
+  });
 });
-});
-
-
-// describe('EditGame', () => {
-//   it('should call editGameById with updated attributes and navigate back when "Edit" button is pressed', async () => {
-//     const navigation = { goBack: jest.fn() };
-//     const route = { params: { gameId: "gameId" } };
-
-//     const { getByText } = render(
-//       <TamaguiProvider config={appConfig}>
-//           <ToastProvider>
-//         <EditGame navigation={navigation} route={route} />
-//         </ToastProvider>
-//       </TamaguiProvider>
-//     );
-  
-//     // This is just to verify that the component renders without throwing any errors
-//     expect(getByText("Edit")).toBeTruthy();
-
-
-
-    // const { getByText, getByTestId } = render(
-    //   <TamaguiProvider config={appConfig}>
-    //     <EditGame navigation={navigation} route={route} />
-    //   </TamaguiProvider>
-    // );
-  
-    // // getByTestId to select the input field
-    // fireEvent.changeText(getByTestId("titleInput"), "New Title");
-    // fireEvent.press(getByText("Edit"));
-  
-    // await waitFor(() => {
-    //   expect(mockEditGameById).toHaveBeenCalledWith(
-    //     "gameId",
-    //     "New Title", // only updating title
-    //     expect.any(Date),
-    //     mockSelectedMyGame.address,
-    //     mockSelectedMyGame.city,
-    //     mockSelectedMyGame.state,
-    //     mockSelectedMyGame.zip,
-    //     mockSelectedMyGame.sport.name,
-    //     expect.any(Number), 
-    //     mockSelectedMyGame.maxPlayers.toString(),
-    //     mockSelectedMyGame.description,
-    //   );
-    // });
-  
-    // expect(navigation.goBack).toHaveBeenCalled();
-//   });
-// });
   
 
 
