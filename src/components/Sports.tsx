1c990f780e78c0f757291268ac33f26b66fbcd92
Madeline Estey <madeline.f.estey@gmail.com>
2024-03-02 10:39:06 -0500

    Trying to fix issue with sports array not updating, still stick

--- src/components/Sports.tsx
+++ src/components/Sports.tsx
@@ -1,18 +1,23 @@
 import { View, Text } from "react-native";
 import { Sport as SportType } from "../lib/types";
 import Sport from "./Sport";
+import useMutationUser from "../hooks/use-mutation-user";
+import { useEffect } from "react";
 
 export default function Sports({ sports }: { sports: any }) {
+
+  console.log("sports");
+  console.log(sports);
   return (
     <View>
       <Text className="text-lg font-bold" style={{paddingLeft: 12}}>My Sports</Text>
       <View>
-        {sports ? (
+        {sports && sports.length > 0 ? (
           sports.map((sport: SportType) => (
             <Sport key={sport.id} sport={sport} />
           ))
         ) : (
-          <Text className="text-lg">No sports yet</Text>
+          <Text className="text-lg" style={{ paddingLeft: 12 }}>No sports yet</Text>
         )}
       </View>
     </View>
