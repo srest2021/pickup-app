77697606af4db0bdfe34ad90c21537372f9a76fa
Sofia Rest <srest2021@gmail.com>
2024-03-03 13:19:51 -0500

    changed edit profile inputs to tamagui

--- src/components/user/EditProfile.tsx
+++ src/components/user/EditProfile.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { ScrollView, View } from "react-native";
-import { Input } from "react-native-elements";
+//import { Input } from "react-native-elements";
 import Avatar from "./Avatar";
-import { Button, Text, YStack } from "tamagui";
+import { Input, Button, Text, YStack, XStack, Label, TextArea } from "tamagui";
 import { useStore } from "../../lib/store";
 import useMutationUser from "../../hooks/use-mutation-user";
 
@@ -30,7 +30,7 @@ export default function EditProfile() {
             backgroundColor: "#ffffff",
           }}
         >
-          <View className="self-stretch py-4">
+          {/* <View className="self-stretch py-4">
             <Input label="Email" value={session?.user?.email} disabled />
           </View>
 
@@ -48,7 +48,56 @@ export default function EditProfile() {
               value={bio}
               onChangeText={(text: string) => setBio(text)}
             />
-          </View>
+          </View> */}
+
+          <XStack space="$5" alignItems="center" justifyContent="space-between">
+              <Label size="$5" color={"#08348c"}>
+                Email
+              </Label>
+              <Input
+                disabled={true}
+                flex={1}
+                size="$5"
+                value={session?.user?.email}
+              />
+          </XStack>
+
+          <XStack space="$5" alignItems="center" justifyContent="space-between">
+              <Label size="$5" color={"#08348c"}>
+                Display Name
+              </Label>
+              <Input
+                flex={1}
+                size="$5"
+                value={displayName}
+                onChangeText={(text: string) => setDisplayName(text)}
+              />
+          </XStack>
+          
+          {/* <XStack space="$5" alignItems="center" justifyContent="space-between">
+              <Label size="$5" color={"#08348c"}>
+                Bio
+              </Label>
+              <Input
+                flex={1}
+                size="$5"
+                value={bio}
+                onChangeText={(text: string) => setBio(text)}
+              />
+          </XStack> */}
+
+            <YStack space="$1">
+              <Label size="$5" color={"#08348c"}>
+              Bio
+              </Label>
+              <TextArea
+                size="$5"
+                placeholder="Enter your game details (optional)"
+                testID="descriptionInput"
+                value={bio}
+                onChangeText={(text: string) => setBio(text)}
+              />
+            </YStack>
 
           <View className="items-center">
             <Avatar
