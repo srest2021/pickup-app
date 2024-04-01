3f5e89713ef8792d2d42a04782ac47f77a786767
Clarissa <msclarissa8@yahoo.com>
2024-04-01 16:57:41 -0400

    friend request button logic

--- src/components/user/OtherProfile.tsx
+++ src/components/user/OtherProfile.tsx
@@ -6,6 +6,7 @@ import { useStore } from "../../lib/store";
 import { Dimensions } from "react-native";
 import { ToastViewport, useToastController } from "@tamagui/toast";
 import { ToastDemo } from "../Toast";
+import useMutationUser from "../../hooks/use-mutation-user";
 
 // Get the height of the screen
 const windowHeight = Dimensions.get("window").height;
@@ -21,13 +22,22 @@ export default function OtherProfile({ navigation }: { navigation: any }) {
     state.user,
   ]);
   const toast = useToastController();
+  const { addFriendRequest } = useMutationUser();
 
   // Returns true if the user has requested
-  function handleRequestLogic(): boolean {
+  const handleRequestLogic = async () => {
     // Send friend request
-    //TODO
-    return false;
-  }
+    // using !, otherUser should never be null if this page appears.
+    const friendRequest = await addFriendRequest(otherUser!.id);
+
+    if (friendRequest) {
+      toast.show("Success!", {
+        message: "Request sent.",
+      });
+    }
+
+    // No navigation, since user can choose to continue viewing the profile.
+  };
 
   return (
     <View style={{ flex: 1 }}>
