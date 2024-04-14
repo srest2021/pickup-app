9671f7d04d8947debc4b1812a1d8ab456f202c3c
Clarissa <msclarissa8@yahoo.com>
2024-04-14 14:57:52 -0400

    removed duplicated stack screen

--- src/components/MyGamesNavigator.tsx
+++ src/components/MyGamesNavigator.tsx
@@ -98,22 +98,6 @@ export default function MyGamesNavigator() {
           },
         }}
       />
-
-      <Stack.Screen
-        name="OtherProfileView"
-        component={OtherProfile}
-        options={{
-          title: "Other Profile",
-          headerShown: true,
-          headerStyle: {
-            backgroundColor: "#08348c",
-          },
-          headerTitleStyle: {
-            color: "#ffffff",
-          },
-          headerTintColor: "#ffffff",
-        }}
-      />
     </Stack.Navigator>
   );
 }
