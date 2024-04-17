9448dcc0984d461d0520f694d5a36a65cbd5b7ac
Sofia Rest <srest2021@gmail.com>
2024-04-17 11:15:05 -0400

    ready for pr

--- supabase/migrations/20240307030811_game_visibility.sql
+++ supabase/migrations/20240307030811_game_visibility.sql
@@ -230,7 +230,8 @@ create policy "Users can delete their own games." on games
 create table joined_game (
   "id" "uuid" primary key unique not null default "gen_random_uuid"(),
   "player_id" "uuid" references "profiles" not null,
-  "game_id" "uuid" references "games" on delete cascade not null
+  "game_id" "uuid" references "games" on delete cascade not null,
+  "plus_one" boolean not null
 );
 
 alter table joined_game
@@ -335,7 +336,8 @@ create table game_requests (
   "id" "uuid" primary key unique not null default "gen_random_uuid"(),
   "created_at" timestamp with time zone default "now"() not null,
   "game_id" "uuid" references "games" on delete cascade not null,
-  "player_id" "uuid" references "profiles" not null
+  "player_id" "uuid" references "profiles" not null,
+  "plus_one" boolean not null
 );
 
 create index gameid_gr
