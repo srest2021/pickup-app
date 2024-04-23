d75bd280eb59de3dba3cd97b770a838eedb7c993
Sofia Rest <srest2021@gmail.com>
2024-04-23 15:39:40 -0400

    fix profile avatar margin

--- src/components/user/Profile.tsx
+++ src/components/user/Profile.tsx
@@ -1,8 +1,8 @@
 import { supabase } from "../../lib/supabase";
-import { ScrollView, View, Text } from "react-native";
+import { ScrollView, Text } from "react-native";
 import Avatar from "./Avatar";
 import Sports from "./Sports";
-import { Button, Card, SizableText, YStack } from "tamagui";
+import { Button, Card, SizableText, YStack, View, H4 } from "tamagui";
 import { useStore } from "../../lib/store";
 import { Dimensions } from "react-native";
 import { Edit3, Loader } from "@tamagui/lucide-icons";
@@ -64,63 +64,41 @@ export default function Profile({ navigation }: { navigation: any }) {
     setLoading(false);
   };
 
-  // Get the height of the screen
+  // Calculate the banner height and the top margin for the avatar
   const windowHeight = Dimensions.get("window").height;
-
-  // Calculate the height for the top third
-  const topThirdHeight = windowHeight / 5;
+  const bannerHeight = windowHeight / 5;
+  const avatarHeight = 170;
+  const paddingHeight = 39;
+  const bannerMargin = bannerHeight - avatarHeight / 2 - paddingHeight;
 
   return (
-    <View style={{ flex: 1, backgroundColor: "#f2f2f2" }}>
+    <View flex={1} backgroundColor="#f2f2f2">
       <ToastViewport />
       <ToastDemo />
-      <View
-        style={{ flex: 1, backgroundColor: "#08348c", flexDirection: "column" }}
-      />
-      <View style={{ flex: 1, flexDirection: "column" }} />
-      <ScrollView
-        style={{ flex: 1, position: "absolute", width: "100%", height: "100%" }}
-        contentContainerStyle={{
-          flexGrow: 1,
-          justifyContent: "space-between",
-          backgroundColor: "#f2f2f2",
-        }}
-      >
-        <View
-          style={{
-            backgroundColor: "#08348c",
-            height: topThirdHeight,
-            width: "100%",
-            flexDirection: "row",
-            justifyContent: "flex-end",
-            alignItems: "flex-start",
-            padding: 12,
-          }}
-        >
-          <Button
-            icon={loading ? Loader : Edit3}
+      {!user ? (
+        <View flex={1}>
+          <View flex={1} backgroundColor="#08348c" flexDirection="column" />
+          <View flex={1} flexDirection="column" />
+          <ScrollView
             style={{
-              width: 55,
-              height: 60,
-              borderColor: "#ffffff",
-              color: "#ffffff",
-              borderRadius: 50,
+              flex: 1,
+              position: "absolute",
+              width: "100%",
+              height: "100%",
             }}
-            theme="active"
-            disabled={loading}
-            onPress={() => navigation.navigate("EditProfile")}
-            size="$6"
-            variant="outlined"
-          ></Button>
-        </View>
-
-        <View className="p-12">
-          {user ? (
-            <View>
-              <View
-                className="items-center mb-10"
-                style={{ marginTop: -topThirdHeight / 1.05 }}
-              >
+            contentContainerStyle={{
+              flexGrow: 1,
+              justifyContent: "space-between",
+              backgroundColor: "#f2f2f2",
+            }}
+          >
+            <View
+              backgroundColor="#08348c"
+              height={bannerHeight}
+              padding="$7"
+              overflow="visible"
+            >
+              <View marginTop={bannerMargin}>
                 <Avatar
                   url={user.avatarUrl}
                   user={user}
@@ -128,54 +106,89 @@ export default function Profile({ navigation }: { navigation: any }) {
                   allowUpload={false}
                 />
               </View>
-
-              <View className="self-stretch py-0">
-                <Text className="text-2xl text-center">
-                  {user.displayName ? user.displayName : "No display name yet"}
-                </Text>
-              </View>
-              <View className="self-stretch py-2">
-                <Text className="text-xl text-center">@{user.username}</Text>
-              </View>
-
-              <YStack paddingTop="$3" paddingBottom="$4">
-                <Card elevate size="$5">
-                  <View marginLeft={25} marginRight={25}>
-                    <SizableText
-                      size="$5"
-                      fontWeight="500"
-                      paddingTop="$3"
-                      paddingBottom="$3"
-                    >
-                      {user.bio ? user.bio : "No bio yet"}
-                    </SizableText>
-                  </View>
-                </Card>
-              </YStack>
-
-              <Sports sports={userSports} otherUser={false} />
-
-              <AddSport />
             </View>
-          ) : (
-            <Text>No user on the session.</Text>
-          )}
 
-          <YStack space="$6" paddingTop="$5" alignItems="center">
             <Button
+              icon={loading ? Loader : Edit3}
+              position="absolute"
+              alignSelf="flex-end"
+              right="$7"
+              top="$7"
+              style={{
+                padding: "$4",
+                height: 50,
+                borderColor: "#ffffff",
+                color: "#ffffff",
+                borderRadius: 50,
+              }}
+              theme="active"
+              disabled={loading}
+              onPress={() => navigation.navigate("EditProfile")}
+              size="$6"
               variant="outlined"
-              onPress={() => handleLogOut()}
-              size="$5"
-              color="#ff7403"
-              borderColor="#ff7403"
-              backgroundColor={"#ffffff"}
-              width="100%"
-            >
-              Log Out
-            </Button>
-          </YStack>
+            ></Button>
+
+            <View padding="$7">
+              <YStack space="$4" flex={1}>
+                <YStack
+                  marginTop={bannerMargin + 15}
+                  space="$1"
+                  paddingVertical="$3"
+                  flex={1}
+                >
+                  <SizableText size="$7" textAlign="center">
+                    {user.displayName
+                      ? user.displayName
+                      : "No display name yet"}
+                  </SizableText>
+
+                  <SizableText size="$5" textAlign="center">
+                    @{user.username}
+                  </SizableText>
+                </YStack>
+
+                <YStack>
+                  <Card elevate size="$5">
+                    <View marginLeft={25} marginRight={25}>
+                      <SizableText
+                        size="$5"
+                        fontWeight="500"
+                        paddingTop="$3"
+                        paddingBottom="$3"
+                      >
+                        {user.bio ? user.bio : "No bio yet"}
+                      </SizableText>
+                    </View>
+                  </Card>
+                </YStack>
+
+                <YStack>
+                  <Sports sports={userSports} otherUser={false} />
+                  <AddSport />
+                </YStack>
+
+                <YStack space="$6" alignItems="center" paddingTop="$3">
+                  <Button
+                    variant="outlined"
+                    onPress={() => handleLogOut()}
+                    size="$5"
+                    color="#ff7403"
+                    borderColor="#ff7403"
+                    backgroundColor={"#ffffff"}
+                    width="100%"
+                  >
+                    Log Out
+                  </Button>
+                </YStack>
+              </YStack>
+            </View>
+          </ScrollView>
         </View>
-      </ScrollView>
+      ) : (
+        <View flex={1} alignSelf="center" justifyContent="center">
+          <H4 textAlign="center">No user on the session.</H4>
+        </View>
+      )}
     </View>
   );
 }
